c0bc88e37a5b1ed7e6b80d868c861996223d416c
ezequielbr19567-hue <ezequielbr19567@gmail.com>
2026-09-23 14:29:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // EDITE APENAS ESTAS DUAS LINHAS PELO PRÓPRIO GITHUB.
 // Supabase > Project Settings > API
 window.CLASH_CONFIG = {
-  SUPABASE_URL: 'https://ftbwybbkpykfjijmmkwd.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_Ln_SGyeKQ_1CEzrvEwhPZg_vZr3fOT7'
+  SUPABASE_URL: 'https://jzgsamwvvhqvynyzvuiw.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_-e3Cx7ypXdLCX79z6cjTGA_pTBS4E-5'
 };
