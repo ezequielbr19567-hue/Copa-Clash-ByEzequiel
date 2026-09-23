25bf97f515de5e3aa644af4f230cf53f128242f7
ezequielbr19567-hue <ezequielbr19567@gmail.com>
2026-09-23 15:34:24 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// EDITE APENAS ESTAS DUAS LINHAS PELO PRÓPRIO GITHUB.
-// Supabase > Project Settings > API
-window.CLASH_CONFIG = {
-  SUPABASE_URL: 'https://jzgsamwvvhqvynyzvuiw.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_-e3Cx7ypXdLCX79z6cjTGA_pTBS4E-5'
-};
