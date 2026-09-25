33854b1b2e48e126c0026e088062bfb4a87fb605
ezequielbr19567-hue <ezequielbr19567@gmail.com>
2026-09-25 17:41:43 -0300

    Delete supabase directory

--- supabase/empate-1-ponto.sql
+++
@@ -1,4 +0,0 @@
--- Atualização opcional dos formatos persistidos; o site já aplica 1 ponto.
-update public.competitions
-set format_config = jsonb_set(coalesce(format_config, '{}'::jsonb), '{points_draw}', '1'::jsonb)
-where kind = 'CUP';
