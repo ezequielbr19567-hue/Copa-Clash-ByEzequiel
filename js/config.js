b16f2469144065e344f6621968918155037c54d5
ezequielbr19567-hue <ezequielbr19567@gmail.com>
2026-09-25 17:55:57 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Configuração pública do projeto Supabase.
+window.CLASH_CONFIG = {
+  SUPABASE_URL: 'https://jzgsamwvvhqvynyzvuiw.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_-e3Cx7ypXdLCX79z6cjTGA_pTBS4E-5'
+};
